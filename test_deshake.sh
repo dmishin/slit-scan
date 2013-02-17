02a87f56b4f29fb735ef28103e140353e0ef4f6b
Dmishin <shintyakov@gmail.com>
2013-02-17 17:25:07 +0400

    Scale option for test

--- test_deshake.sh
+++ test_deshake.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#./slit-scan -s438:117:169:87:10:20:1000  videos/shaky.mp4
-./slit-scan -s876:234:338:174:20:20:1000  videos/shaky.mp4
+./slit-scan -s438:117:169:87:10:20:1000  -k2 videos/shaky.mp4
 
 gpicview shaky.png
