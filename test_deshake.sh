e1d03611fed7d18899e6ee1d76626ce7ef90deb5
Dmishin <shintyakov@gmail.com>
2013-03-23 02:22:06 +0400

    minor

--- test_deshake.sh
+++ test_deshake.sh
@@ -3,3 +3,5 @@
 ./slit-scan -s438:117:169:87:10:20:1000 -S20 videos/shaky.mp4
 
 gpicview shaky.png
+
+#./slit-scan -s438:117:169:87:10:20:1000 -S120 -rh -s18:21:250:250:10:10:1000 -p80 videos/new/утки.mp4
